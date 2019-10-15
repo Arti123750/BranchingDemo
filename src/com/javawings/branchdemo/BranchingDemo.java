c50d505f359b4c15e8e021d84b06da8a06114232
Arti123750 <55482816+Arti123750@users.noreply.github.com>
2019-10-15 12:29:36 +0530

    Update BranchingDemo.java

--- src/com/javawings/branchdemo/BranchingDemo.java
+++ src/com/javawings/branchdemo/BranchingDemo.java
@@ -8,7 +8,7 @@ public class BranchingDemo {
 		System.out.println("Hiii..........!");
 		System.out.println("My New Branch is Created");
 		System.out.println("My name is Arti");
-		System.out.println("My name is Arti Avinash Doifode");****************
+		System.out.println("My name is Arti Avinash Doifode");
 		
 	}
 
