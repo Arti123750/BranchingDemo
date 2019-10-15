e7a33347e5d994a3831e4ccad9491bc1ccf9322f
Arti123750 <bombalearti@gmail.com>
2019-10-15 12:01:54 +0530

    Added branching Demo

---
+++ src/com/javawings/branchdemo/BranchingDemo.java
@@ -0,0 +1,12 @@
+package com.javawings.branchdemo;
+
+public class BranchingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hiii..........!");
+		System.out.println("My New Branch is Created");
+	}
+
+}
